799dc8b201839438dacf57827894cb0c7903e0ea
J.B.C <juanbicortelezzi@gmail.com>
2023-09-18 10:41:00 +1200

    working

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import { serveStatic } from "hono/bun";
 
 const app = new Hono();
 
@@ -8,8 +9,14 @@ const globalState = {
 
 const Button = () => {
   return (
-    <button hx-post="/click" hx-trigger="click" hx-swap="outerHTML">
+    <button
+      hx-post="/click"
+      hx-trigger="click"
+      hx-swap="outerHTML"
+      class="bg-blue-500 border border-black rounded-lg flex w-64 justify-center items-center"
+    >
       {globalState.clicked ? "clicked" : "click me daddy!"}
+      <img class="htmx-indicator w-10 h-10" src="/static/spinner.svg" />
     </button>
   );
 };
@@ -22,7 +29,7 @@ const View = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
         <title>My Website</title>
-        {/* <link rel="stylesheet" href="./style.css" /> */}
+        <link rel="stylesheet" href="/static/index.css" />
         {/* <link rel="icon" href="./favicon.ico" type="image/x-icon" /> */}
       </head>
 
@@ -35,9 +42,20 @@ const View = () => {
   );
 };
 
+app.use("/static/index.css", async (c) => {
+  const file = Bun.file("./static/index.css");
+  const t = await file.text();
+  console.log(t);
+  c.header("Content-Type", "text/css");
+  return c.text(t);
+});
+
+app.use("/static/*", serveStatic({ root: "." }));
+
 app.get("/", (c) => c.html(<View />));
-app.post("/click", (c) => {
+app.post("/click", async (c) => {
   globalState.clicked = !globalState.clicked;
+  await Bun.sleep(1000);
   return c.html(<Button />);
 });
 
